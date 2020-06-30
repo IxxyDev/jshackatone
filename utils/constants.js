87ed1170e47fcd85cada6198d36d416be8af5de4
Dmitrii Belich <dim1999@mail.ru>
2020-06-30 23:14:40 +0300

    feat: добавление блоков текста и тайтлов

--- utils/constants.js
+++ utils/constants.js
@@ -3,8 +3,17 @@ import Section from "../components/Section.js";
 import TextContainer from "../components/TextContainer.js";
 import Title from "../components/Title.js";
 export const header = new Title(initialData.mainTitle, '.main-title', '.icon');
+const textRenderer = function(evt) {
+  const addedText = new TextContainer('', 'article', textRenderer, titleRenderer).getTextContainer()
+  section.addnewItem(addedText, evt.target.closest('.section'));
+}
+const titleRenderer = function(evt) {
+  const addedText = new TextContainer('', 'title', textRenderer, titleRenderer).getTextContainer()
+  section.addnewItem(addedText, evt.target.closest('.section'));
+}
 export const section = new Section({items: initialData.sections, renderer: function(item) {
-    const newText = new TextContainer(item.text, item.type).getTextContainer()
+    const newText = new TextContainer(item.text, item.type, textRenderer, titleRenderer).getTextContainer()
     this.addItem(newText);
-}}, '.content')
+}}, '.content'
+)
 //export const text = new TextContainer ('asfdsadf', 'article').getTextContainer()
