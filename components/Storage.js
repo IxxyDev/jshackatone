eb6ce1c6a5ff1f30f52a810c3445f3717471a5f3
Dmitrii Belich <dim1999@mail.ru>
2020-07-01 09:49:24 +0300

    feat: добавить работу со storage

---
+++ components/Storage.js
@@ -0,0 +1,32 @@
+import { section } from "../utils/constants";
+
+export default class Storage {
+  constructor({mainTitle, sections}) {
+    this._mainTitle = mainTitle;
+    this._sections = sections;
+  }
+  refreshStorage(key, value) {
+    
+    localStorage[key] = JSON.stringify(value);;
+  }
+  getData(key) {
+    return JSON.parse(localStorage[key])
+  }
+  getInitialData() {
+    const initialStorage = {
+      mainTitle: this.getData('mainTitle'),
+      sections: this.getData('sections')
+    };
+    return initialStorage;
+
+  }
+
+  initialRender() {
+    if (!localStorage.mainTitle) {
+    this.refreshStorage("mainTitle", this._mainTitle);
+    }
+    if (!localStorage.sections) {
+    this.refreshStorage("sections", this._sections) 
+    }
+  }
+}
--- utils/constants.js
+++ utils/constants.js
@@ -2,7 +2,12 @@ import {initialData} from "./data.js"
 import Section from "../components/Section.js";
 import TextContainer from "../components/TextContainer.js";
 import Title from "../components/Title.js";
-export const header = new Title(initialData.mainTitle, '.main-title', '.icon');
+import Storage from "../components/Storage.js";
+/*  localStorage.clear() */ 
+const storage = new Storage (initialData);
+storage.initialRender();
+console.log(storage.getInitialData())
+export const header = new Title(storage.getInitialData().mainTitle, '.main-title', '.icon');
 const textRenderer = function(evt) {
   const addedText = new TextContainer('Введите текст', 'article', textRenderer, titleRenderer).getTextContainer()
   section.addnewItem(addedText, evt.target.closest('.section'));
@@ -11,9 +16,10 @@ const titleRenderer = function(evt) {
   const addedText = new TextContainer('Заголовок', 'title', textRenderer, titleRenderer).getTextContainer()
   section.addnewItem(addedText, evt.target.closest('.section'));
 }
-export const section = new Section({items: initialData.sections, renderer: function(item) {
+export const section = new Section({items: storage.getInitialData().sections, renderer: function(item) {
     const newText = new TextContainer(item.text, item.type, textRenderer, titleRenderer).getTextContainer()
     this.addItem(newText);
 }}, '.content'
 )
-//export const text = new TextContainer ('asfdsadf', 'article').getTextContainer()
+
+
